ffc011e06bf0584b95efbfe5fef39fcc7c042274
Leonardo Cserny <leonardo_cserny@netex.ro>
2018-05-18 18:03:06 +0300

    Initial commit

---
+++ core/src/net/cserny/game/GameScreen.java
@@ -0,0 +1,14 @@
+package net.cserny.game;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class GameScreen extends ScreenAdapter {
+
+    private final NuttyGame game;
+
+    public GameScreen(NuttyGame game) {
+        this.game = game;
+    }
+
+    
+}
---
+++ core/src/net/cserny/game/NuttyGame.java
@@ -0,0 +1,18 @@
+package net.cserny.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
+
+public class NuttyGame extends Game {
+
+	private final AssetManager assetManager = new AssetManager();
+
+	@Override
+	public void create() {
+		setScreen(new LoadingScreen(this));
+	}
+
+	public AssetManager getAssetManager() {
+		return assetManager;
+	}
+}
